abbd802899116a9368d55e4fb99a7d7435168574
Tural Hasanli <tural.hasanli@smoothstack.com>
2021-03-22 10:10:49 -0400

    Adding loger to debug in cloudwatch

--- src/main/java/com/utopia/auth/services/EmailService.java
+++ src/main/java/com/utopia/auth/services/EmailService.java
@@ -1,61 +1,74 @@
 package com.utopia.auth.services;
 
+import com.utopia.auth.models.MailRequest;
+import com.utopia.auth.models.MailResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
-
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import javax.validation.UnexpectedTypeException;
-
+import org.slf4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
-import com.utopia.auth.models.MailRequest;
-import com.utopia.auth.models.MailResponse;
-
-import freemarker.template.Configuration;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
-
 @Service
 public class EmailService {
-	
-	@Autowired
-	private JavaMailSender sender;
-	@Autowired
-	private Configuration config;
-	
-	public MailResponse sendEmail(MailRequest request, Map<String, Object> model) {
-		MailResponse response = new MailResponse();
-		MimeMessage message = sender.createMimeMessage();
-		try {
-			// set mediaType
-			MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
-					StandardCharsets.UTF_8.name());
 
-			Template t = config.getTemplate("email-template.ftl");
-			String html = FreeMarkerTemplateUtils.processTemplateIntoString(t, model);
+  @Autowired
+  private JavaMailSender sender;
 
-			helper.setTo(request.getTo());
-			helper.setText(html, true);
-			helper.setSubject(request.getSubject());
-			helper.setFrom(request.getFrom());
-			sender.send(message);
+  @Autowired
+  private Configuration config;
 
-			response.setMessage("mail send to : " + request.getTo());
-			response.setStatus(Boolean.TRUE);
+  private static final Logger LOGGER = LoggerFactory.getLogger(
+    EmailService.class
+  );
 
-		} catch (UnexpectedTypeException | MessagingException | IOException | TemplateException e) {
-			response.setMessage("Mail Sending failure : "+ e.getMessage());
-			response.setStatus(Boolean.FALSE);
-		}
+  public MailResponse sendEmail(
+    MailRequest request,
+    Map<String, Object> model
+  ) {
+    LOGGER.info("EmailService... check");
+    MailResponse response = new MailResponse();
+    MimeMessage message = sender.createMimeMessage();
+    try {
+      // set mediaType
+      MimeMessageHelper helper = new MimeMessageHelper(
+        message,
+        MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+        StandardCharsets.UTF_8.name()
+      );
 
-		return response;
-	}
-	
-	
+      Template t = config.getTemplate("email-template.ftl");
+      String html = FreeMarkerTemplateUtils.processTemplateIntoString(t, model);
+
+      helper.setTo(request.getTo());
+      helper.setText(html, true);
+      helper.setSubject(request.getSubject());
+      helper.setFrom(request.getFrom());
+      sender.send(message);
+      LOGGER.info("Sent recovery email");
+
+      response.setMessage("mail send to : " + request.getTo());
+      response.setStatus(Boolean.TRUE);
+      LOGGER.info("Sent recovery email");
+    } catch (
+      UnexpectedTypeException
+      | MessagingException
+      | IOException
+      | TemplateException e
+    ) {
+      LOGGER.error("Error recovery email: " + e.getMessage());
+      response.setMessage("Mail Sending failure : " + e.getMessage());
+      response.setStatus(Boolean.FALSE);
+    }
+
+    return response;
+  }
 }
